a7b2d7154b20980532d8cef2b9246580aa01a462
Joy Ying <ying.joy@gmail.com>
2022-08-03 23:15:11 -0700

    node app running

---
+++ public/shaders/vertex.glsl.js
@@ -0,0 +1,11 @@
+const vertexShader = `
+varying vec2 vertexUV;
+varying vec3 vertexNormal;
+
+void main() {
+    vertexUV = uv;
+    vertexNormal = normalize(normalMatrix * normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`
+export default vertexShader;
